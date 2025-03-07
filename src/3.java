4129a3c4bf03af6748f5bf452a304ba77b853bec
faoareba <faoareba@gmx.com>
2025-03-07 06:38:31 +0000

    Add 3.java

---
+++ src/3.java
@@ -0,0 +1,7 @@
+ import java.util.*; 
+public class MathHomework { 
+    public static int getRandomNumber(int min, int max) { 
+        Random rand = new Random(); 
+        return rand.nextInt((max - min) + 1) + min; 
+    } 
+}
