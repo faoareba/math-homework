705d69fa49369bf5b62c6c021057d88817e06496
faoareba <faoareba@gmx.com>
2025-03-08 05:21:29 +0000

    Add 4.java

---
+++ src/4.java
@@ -0,0 +1,11 @@
+ 
+import java.util.Scanner;
+public class MathHomework {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the two numbers: ");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println("Sum of the two numbers is: " + (num1 + num2));
+    }
+}
